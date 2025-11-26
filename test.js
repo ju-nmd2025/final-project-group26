ba5dac52307db541c38d4c893b30a257b8aca70b
Maddiesa <joma24rw@student.ju.se>
2025-11-26 14:37:45 +0100

    gettingTestedPartTwo

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
 function setup() {
   createCanvas(440, 590);
-}
-function draw() {
   background(100, 160, 200);
-  button = createButton("START");
+}
+function createButton(click here) {
   button.position(200, 300);
   button.style("font", "italic bold 20px arial");
+  button.mousePressed(repaint);
+}
+function repaint() {
+  let r = random(255);
+  let g = random(255);
+  let b = random(255);
+  background(r, g, b);
+}
+function mousePressed() {
+  if (mouseX >= 130 && mouseX <= 266 && mouseY >= 182 && mouseY <= 208) {
+    buttonPressed = true;
+    console.log("you did it! 🥂(✿◡‿◡)");
+  } else {
+    buttonPressed = false;
+    console.log("you failed `(*>﹏<*)′");
+  }
 }
