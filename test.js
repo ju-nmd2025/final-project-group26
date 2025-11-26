5254ff0c7886d1a61d858db5b8de2ac69c5e7fb0
Maddiesa <joma24rw@student.ju.se>
2025-11-26 14:26:48 +0100

    ~ ~ "COMMIT_EDITMSG" 10L, 200C git push --forceIamGettingTested

---
+++ test.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(440, 590);
+}
+function draw() {
+  background(100, 160, 200);
+  button = createButton("START");
+  button.position(200, 300);
+  button.style("font", "italic bold 20px arial");
+}
