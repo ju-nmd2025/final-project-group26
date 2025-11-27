b668609c6a9ccc944a9b8215a4551ba54509bcb0
Maddiesa <joma24rw@student.ju.se>
2025-11-27 14:34:33 +0100

    NewColour

--- platform.js
+++ platform.js
@@ -1,13 +1,13 @@
 export let platform = {
-    x: 250,
-    y: 230,
-    w: 80,
-    h: 20,
+  x: 250,
+  y: 230,
+  w: 80,
+  h: 20,
 
-    draw() {
-        push();
-        fill("blue");
-        rect(this.x, this.y, this.w, this.h);
-        pop();
-    },
+  draw() {
+    push();
+    fill(145, 73, 73);
+    rect(this.x, this.y, this.w, this.h);
+    pop();
+  },
 };
