ac3b1ae0be582a358e9fc5d8926b9455fb9e3fa3
Maddiesa <joma24rw@student.ju.se>
2025-12-05 14:29:53 +0100

    AddedGravityToCharacter
    Velocity + Gravity + character.update

--- MaddieMain.js
+++ MaddieMain.js
@@ -4,11 +4,20 @@ class Character {
     this.y = height / 2;
     this.width = 60;
     this.height = 60;
+
+    this.velocity = 0;
+    this.gravity = 0.1;
   }
+
   draw() {
     rect(this.x, this.y, this.width, this.height);
   }
+  update() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+  }
 }
+
 let character;
 function setup() {
   createCanvas(440, 600);
@@ -17,4 +26,5 @@ function setup() {
 function draw() {
   background(155, 203, 45);
   character.draw();
+  character.update();
 }
