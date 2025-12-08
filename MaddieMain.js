224e4f47ecb1ab725b01ef8391940f255ee57c8e
Maddiesa <joma24rw@student.ju.se>
2025-12-08 15:54:31 +0100

    Remade functions + score added
    Moved order of variables & character class

--- MaddieMain.js
+++ MaddieMain.js
@@ -1,9 +1,23 @@
+// variables
+let character;
+let platforms = []; // empty platform array
+let gap;
+let score = 0;
+
+let gameState = "start"; // "start", "playing", "gameover"
+let startButton;
+let gameOverButton;
+
+// character class
 class Platform {
   constructor(x, y, w = 100, h = 20) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+    this.velocity = 1; // fall speed
+    this.gravity = 0.1;
+    this.jumpStrength = 7;
   }
 
   draw() {
@@ -14,152 +28,152 @@ class Platform {
   }
 }
 
-class Character {
-  constructor(x = 200, y = 300, w = 60, h = 60) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.velocity = 0; //movespeed
-    this.gravity = 0.9;
-    this.jumpStrength = 7;
-  } 
-
   jump() {
     this.velocity -= this.jumpStrength;
   }
 
-  draw() {
-    push();
-    fill(157, 73, 73);
-    rect(this.x, this.y, this.width, this.height, 10);
-    pop();
-  }
-
-  update() {
+  update(platforms) {
     this.velocity += this.gravity;
     this.y += this.velocity;
 
-    if (keyIsDown(LEFT_ARROW)) || keyIsDown(65) this.x -= this.velocity;
-    if (keyIsDown(RIGHT_ARROW)) || keyIsDown(68) this.x += this.velocity;
+    if (this.velocity < -9) this.velocity = -9;
 
-    this.x = constrain(this.x, 0, 440 - this.w); // keep inside canvas
+    if (keyIsDown(LEFT_ARROW)) this.x -= 4;
+    if (keyIsDown(RIGHT_ARROW)) this.x += 4;
+
+    if (this.x + this.width < 0) this.x = width;
+    if (this.x > width) this.x = -this.width;
+
+    for (let platform of platforms) {
+      if (
+        this.y + this.height >= platform.y &&
+        this.y + this.height <= platform.y + platform.height &&
+        this.velocity > 0
+      ) {
+        let minX = platform.x - this.width;
+        let maxX = platform.x + platform.width;
+
+        if (this.x >= minX && this.x <= maxX) {
+          this.jump();
+        }
+      }
+    }
+    
+// game over if fall too low
+    if (this.y > platforms[0].y + 600) {
+      gameState = "gameover";
+    }
   }
-}
-// variables
-let character;
-let platforms = []; // empty platform array
-
+// GAME SETUP //
 function setup() {
   createCanvas(440, 600);
-  character = new Character();
+ setupStartScreen();
+}
+
+// initialise game //
+function setupGame() {
+  character = new Character(200, 300, 60, 60);
+  platforms = []; 
+  score = 0;
 
   let platformCount = 5;
-  let gap = height / platformCount;
+  gap = height / platformCount;
+
   for (let i = 1; i < 5; i++) {
     platforms.push(new Platform(random(width), height - i * gap));
   }
 }
-function initPlatforms() {
-  platforms = [];
-  let firstW = random(platformWidthRange[0], platformWidthRange[1]);
-  platforms.push(
-    neww Platform(random(0, canvasWidth, firstW), floor - 80, firstW,20
-  ));
-  for (let i = 1; i < platformCount; i++) {
-    let w = random(platformWidthRange[0], platformWidthRange[1]);
-    let x = random(0, canvasWidth - w);
-    let y =
-      platforms[i - 1].y - random(
-        platformVerticalSpacing[0],
-        platformVerticalSpacing[1]);
-    platforms.push(new Platform(x, y, w, 20));
-  }
-}
-function showStartScreen() {
-   background(100, 160, 200);
-  textSize(32);
-  textStyle(BOLD);
-  fill(255);
-  text("hmm...", 40, 100);
 
-  button = createButton("START 😎");
-  button.position(170, 270);
-  button.size(150, 70);
-  button.style("font", "bold 26px verdana");
-  button.style("border-radius", "15px");
-  button.mousePressed(startGame);
-}
-function startGame() {
-   gameStarted = true;
-  gameOverState = false;
-  platformsFalling = false;
-  button.remove();
-  character = new Character(200, 300, 60, 60);
-  initPlatforms();
-  loop();
-}
-function isOnAnyPlatform() {
-   for (let plat of platforms) {
-    const horizontallyAligned =
-      character.x + character.w > plat.x && character.x < plat.x + plat.w;
-    const verticallyAligned =
-      character.y + character.h >= plat.y - tolerance &&
-      character.y + character.h <= plat.y + tolerance;
-    if (horizontallyAligned && verticallyAligned) return plat;
-  }
-  return null;
-}
-function gameOver() {
-  gameOverState = true;
-  noLoop();
-  background(0);
-  fill(255);
-  textSize(28);
-  textAlign(CENTER);
-  text("you just lost the game (✿◡‿◡)", canvasWidth / 2, canvasHeight / 2);
-  textSize(16);
-  text("Press SPACE to restart", canvasWidth / 2, canvasHeight / 2 + 40);
-}
-
+// drawing loop //
 function draw() {
-  if (!gameStarted) return;
-  background(155, 203, 45);
-
-  if (gameOverState) {
-    gameOver();
+ if (gameState === "start") {
+    drawStartScreen();
     return;
   }
 
-  character.update(plaforms);
-  for (let plat of platforms) {
-    plat.draw();
-
-  if (character.velocity < platforms[platforms.length - 1].y + 200) {
-    platforms.push(new Platform(random(width), platforms[platforms-length - 1].y - gap));
+  if (gameState === "gameover") {
+    drawGameOverScreen();
+    return;
   }
 
-    let p = platforms[platforms.length - 1];
-    platforms.push(new Platform(random(width), p.y - random(80, 120)));
+  // PLay state //
+  translate(0, width / 2 - character.y);
+  character.update(platforms);
+  character.draw();
+
+  for (let plat of platforms);
+    plat.draw();
+  
+    // new platforms as character goes up //
+    if (character.y < platforms[platforms.length - 1].y + 200) {
+      platforms.push(new Platform(random(width), platforms[platforms.length - 1].y - gap));
+    }
+
+    // removing old plats and score go up //
+    if (platforms[0].y > character.y + 400) {
+      platforms.shift();
+      score++;
+    }
+
+    // draw score //
+    push();
+    fill(0);
+    textSize(24);
+    textAlign(CENTER);
+    text(score, width / 2, character.y - 150);
+  }
+
+  // start screen //
+  function setupStartScreen() {
+// creation of start button //
+  startButton = createButton("START 😎");
+  startButton.position(170, 270);
+  startButton.size(150, 70);
+  startButton.style("font", "bold 26px verdana");
+  startButton.style("border-radius", "15px");
+  button.mousePressed(() => {
+    gameState = "playing";
+    startButton.hide();
+  });
+}
+
+function drawStartScreen() {
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(255);
+  text("game title", width / 2, height / 3);
+}
+
+
+  // game over screen //
+function drawGameOverScreen() {
+  background(0);
+  textAlign(CENTER, CENTER);
+  textSize(28);
+  fill(255);
+  text("you just lost the game (✿◡‿◡)", width / 2, height / 2);
+  textSize(16);
+  text("SCORE:" + score, width / 2, height / 2 + 30);
+
+  if (!gameOverButton) {
+    gameOverButton = createButton("RESTART 🔄");
+    gameOverButton.position(width / 2 - 75, height / 2 + 70);
+    gameOverButton.size(150, 50);
+    gameOverButton.style("font", "bold 20px verdana");
+    gameOverButton.style("border-radius", "10px");
+    gameOverButton.mousePressed(() => {
+      setupGame();
+      gameOverButton.remove();
+      gameOverButton = null;
+      gameState = "playing";
+    });
   }
 }
 
-translate(0, width / 2 - doodler.y);
+// jump input //
 
-function keyPressed() {
-  if (!gameStarted) return; // Ignore before game start, prevents restart
-
-  if (gameOverState && keyCode === "32") { // game is over and spacebar restarts 
-      startGame();
-      return;
-    }
+function keyPressed() { // if dead and space press, restart game
+  if (gameState === "playing" && keyCode === "  "){
+    character.jump();
   }
-
-  //checking if floor deadly, y+h feet of character, tolerance few pixels of wiggle room
-let onFloor = character.y + character.h >= floor - tolerance &&
-character.y + character.h <= floor + tolerance;
-let onPlatform = isOnAnyPlatform(); // cehcks platform collision
-
-if (onFloor || onPlatform) {
-  character.jump();
-} // character on floor or platform jumpspeed, only jump trigger no mid air jump 
+}
