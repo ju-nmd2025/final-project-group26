51e1e93b5a2c5d3a79d41a6c04515111ad2f43e3
Patrik-RaS <patrik.pato.madarcik@gmail.com>
2025-12-08 15:53:51 +0100

    putting GameVariables into a comment for now
    just to not get lost in the process of coding for now :)) and separate already divided with not yet divided codes into sheets

--- GameGroup26.js
+++ GameGroup26.js
@@ -17,25 +17,28 @@
 //   }
 // }
 
-let canvasWidth = 440;
-let canvasHeight = 600;
-let floor = 530; // you'll die
-let character;
-const moveSpeed = 5;
-const jumpSpeed = -15;
-const tolerance = 5;
-const platformFallSpeed = 1;
+// -------------------------------------------------------
+// GAME VARIABLES & CONFIGURATION
+// -------------------------------------------------------
+let canvasWidth = 440;          // Width of the game window
+let canvasHeight = 600;         // Height of the game window
+let floor = 530; // you'll die. // Y position of the deadly floor line
+let character;                  // Will store the Character object
+const moveSpeed = 5;            // How fast the character moves left/right
+const jumpSpeed = -15;          // Upward jump velocity (negative goes up)
+const tolerance = 5;            // Collision leniency for platform landing
+const platformFallSpeed = 1;    // How fast platforms move downward
 
-let gameStarted = false;
-let gameOverState = false;
-let button;
+let gameStarted = false;        // True only after clicking START
+let gameOverState = false;      // True when player dies
+let button;                     // The START button element
 
-let platforms = [];
-const platformCount = 5;
-const platformWidthRange = [60, 120];
-const platformVerticalSpacing = [80, 150];
+let platforms = [];             // Holds all platform objects
+const platformCount = 5;        // How many platforms exist
+const platformWidthRange = [60, 120];   // Min/max platform width
+const platformVerticalSpacing = [80, 150]; // Min/max vertical distance
 
-let platformsFalling = false;
+let platformsFalling = false;   // When true, platforms begin to fall
 
 function initPlatforms() {
   platforms = [];
