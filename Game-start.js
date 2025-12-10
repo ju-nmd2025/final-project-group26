371fb63c93ac3ff1eea2d0eb68569cd257fcf91f
Patrik-RaS <patrik.pato.madarcik@gmail.com>
2025-12-10 13:41:24 +0100

    Delete Game-start.js
    cleaning up branch

--- Game-start.js
+++
@@ -1,9 +0,0 @@
-function startGame() {
-  gameStarted = true;
-  gameOverState = false;
-  platformsFalling = false;
-  button.remove();
-  character = new Character(175, 50, 50, 50); // reset
-  initPlatforms();
-  loop();
-}
