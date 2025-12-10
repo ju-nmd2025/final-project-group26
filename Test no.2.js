97fb3eeee5d7a922812d37579ea4af7975927bd2
Patrik-RaS <patrik.pato.madarcik@gmail.com>
2025-12-10 12:33:06 +0100

    creating import export for character

--- Test no.2.js
+++ Test no.2.js
@@ -11,83 +11,8 @@ let gameOverButton;
 // -------------------------------------------------------------
 // CHARACTER CLASS
 // -------------------------------------------------------------
-class Character {
-  constructor() {
-    this.width = 45;
-    this.height = 45;
-
-    this.x = width / 2 - this.width / 2;
-    this.y = height - this.height - 10;
-
-    this.velocity = 0;
-    this.gravity = 0.3;
-    this.jumpStrength = 9;
-
-    this.started = false;
-    this.firstJumpEase = 50;
-  }
-
-  draw() {
-    fill(139, 69, 19);
-    rect(this.x, this.y, this.width, this.height, 10);
-  }
-
-  jump() {
-    if (!this.started) {
-      this.started = true;
-      this.firstJumpEase = 0;
-    } else {
-      this.velocity = -this.jumpStrength;
-    }
-  }
-
-  update(platforms) {
-    // Smooth first jump
-    if (this.started && this.firstJumpEase < 1) {
-      this.firstJumpEase += 0.03;
-      this.velocity = -this.jumpStrength * this.firstJumpEase;
-    } else if (this.started) {
-      this.velocity += this.gravity;
-    }
-
-    this.y += this.velocity;
-
-    // Left / Right
-    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) this.x -= 7;
-    if (keyIsDown(RIGHT_ARROW)|| keyIsDown(68)) this.x += 7;
-
-    // Wrap screen
-    if (this.x + this.width < 0) this.x = width;
-    if (this.x > width) this.x = -this.width;
-
-    // Platform collisions
-    if (this.started) {
-      for (let platform of platforms) {
-        if(platform.broken) continue; //skip broken platform
-        if (
-          this.y + this.height >= platform.y &&
-          this.y + this.height <= platform.y + platform.height &&
-          this.velocity > 0
-        ) {
-          let minX = platform.x - this.width;
-          let maxX = platform.x + platform.width;
-
-          if (this.x >= minX && this.x <= maxX) {
-            this.velocity = -this.jumpStrength;
-
-            if(platform.type === "breakable") platform.break();
-          }
-        }
-      }
-    }
-
-    // Game over if fall too low
-    if (this.y > height + 200) {
-      gameState = "gameover";
-    }
-  }
-}
-
+import { Character } from "./character.js";
+  
 // -------------------------------------------------------------
 // MOVING PLATFORM CLASS
 // -------------------------------------------------------------
--- character.js
+++ character.js
@@ -17,38 +17,112 @@
 //   }
 // }
 
-export default class Character {
+// export class Character {
+//   constructor() {
+//     this.width = 45; // character side
+//     this.height = 45; // character side
+
+//     this.x = width / 2 - this.width / 2;
+//     this.y = height - this.height - 10; // 10 px margin from bottom
+
+//     this.velocity = 0; // fall speed
+//     this.gravity = 0.05; // how much boune on platform, lower number = weaker
+//     this.jumpStrength = 4;
+
+//     this.started = false; // jump not start
+//     this.firstJumpEase = 500; // easing factor for smooth first jump
+//   }
+
+//   draw() {
+//     fill(139, 69, 19);
+//     rect(this.x, this.y, this.width, this.height, 10); // last number radius on corner
+//   }
+
+//   jump() {
+//     if (!this.started) {
+//       this.started = true;
+//       this.firstJumpEase = 0; // starting ease jump
+//     } else {
+//       this.velocity = -this.jumpStrength;
+//     }
+//   }
+
+//   update(platforms) {
+//     // smooth first jump
+//     if (this.started && this.firstJumpEase < 1) {
+//       this.firstJumpEase += 0.03;
+//       this.velocity = -this.jumpStrength * this.firstJumpEase;
+//     } else if (this.started) {
+//       this.velocity += this.gravity;
+//     }
+
+//     this.y += this.velocity;
+
+//     // left right move
+//     if (keyIsDown(LEFT_ARROW)) this.x -= 4;
+//     if (keyIsDown(RIGHT_ARROW)) this.x += 4;
+
+//     // screen wrap
+//     if (this.x + this.width < 0) this.x = width;
+//     if (this.x > width) this.x = -this.width;
+
+//     // platform collis. after jump starts
+//     if (this.started) {
+//       for (let platform of platforms) {
+//         if (
+//           this.y + this.height >= platform.y &&
+//           this.y + this.height <= platform.y + platform.height &&
+//           this.velocity > 0
+//         ) {
+//           let minX = platform.x - this.width;
+//           let maxX = platform.x + platform.width;
+//           if (this.x >= minX && this.x <= maxX) {
+//             this.velocity = -this.jumpStrength;
+//           }
+//         }
+//       }
+//     }
+
+//     // game over if fall too low
+//     if (this.y > height + 200) {
+//       gameState = "gameover";
+//     }
+//   }
+// }
+
+
+export class Character {
   constructor() {
-    this.width = 45; // character side
-    this.height = 45; // character side
+    this.width = 45;
+    this.height = 45;
 
     this.x = width / 2 - this.width / 2;
-    this.y = height - this.height - 10; // 10 px margin from bottom
+    this.y = height - this.height - 10;
 
-    this.velocity = 0; // fall speed
-    this.gravity = 0.05; // how much boune on platform, lower number = weaker
-    this.jumpStrength = 4;
+    this.velocity = 0;
+    this.gravity = 0.3;
+    this.jumpStrength = 9;
 
-    this.started = false; // jump not start
-    this.firstJumpEase = 500; // easing factor for smooth first jump
+    this.started = false;
+    this.firstJumpEase = 50;
   }
 
   draw() {
     fill(139, 69, 19);
-    rect(this.x, this.y, this.width, this.height, 10); // last number radius on corner
+    rect(this.x, this.y, this.width, this.height, 10);
   }
 
   jump() {
     if (!this.started) {
       this.started = true;
-      this.firstJumpEase = 0; // starting ease jump
+      this.firstJumpEase = 0;
     } else {
       this.velocity = -this.jumpStrength;
     }
   }
 
   update(platforms) {
-    // smooth first jump
+    // Smooth first jump
     if (this.started && this.firstJumpEase < 1) {
       this.firstJumpEase += 0.03;
       this.velocity = -this.jumpStrength * this.firstJumpEase;
@@ -58,17 +132,18 @@ export default class Character {
 
     this.y += this.velocity;
 
-    // left right move
-    if (keyIsDown(LEFT_ARROW)) this.x -= 4;
-    if (keyIsDown(RIGHT_ARROW)) this.x += 4;
+    // Left / Right
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) this.x -= 7;
+    if (keyIsDown(RIGHT_ARROW)|| keyIsDown(68)) this.x += 7;
 
-    // screen wrap
+    // Wrap screen
     if (this.x + this.width < 0) this.x = width;
     if (this.x > width) this.x = -this.width;
 
-    // platform collis. after jump starts
+    // Platform collisions
     if (this.started) {
       for (let platform of platforms) {
+        if(platform.broken) continue; //skip broken platform
         if (
           this.y + this.height >= platform.y &&
           this.y + this.height <= platform.y + platform.height &&
@@ -76,14 +151,17 @@ export default class Character {
         ) {
           let minX = platform.x - this.width;
           let maxX = platform.x + platform.width;
+
           if (this.x >= minX && this.x <= maxX) {
             this.velocity = -this.jumpStrength;
+
+            if(platform.type === "breakable") platform.break();
           }
         }
       }
     }
 
-    // game over if fall too low
+    // Game over if fall too low
     if (this.y > height + 200) {
       gameState = "gameover";
     }
