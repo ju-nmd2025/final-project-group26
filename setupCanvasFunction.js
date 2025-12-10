5403088e5a813814a8e6781752140e5bc150d564
Patrik-RaS <patrik.pato.madarcik@gmail.com>
2025-12-10 13:39:14 +0100

    Delete setupCanvasFunction.js
    cleaning up branch

--- setupCanvasFunction.js
+++
@@ -1,6 +0,0 @@
-//THE SETUP FUNCTION (creates canvas + background)
-function setup() {
-  createCanvas(canvasWidth, canvasHeight); //  Creates the canvas where the game is drawn
-  character = new Character(175, 50, 50, 50);   //  Create the player character
-  showStartScreen();   //  Display the start screen first
-}
